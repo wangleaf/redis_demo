4fce682fba1e54153b6e60a18c5d30c4ded28ca4
fkh <i_fankehan@shizhuang-inc.com>
2024-07-08 10:02:15 +0800

    opsForValue

--- src/test/java/com/example/redis_demo/RedisDemoApplicationTests.java
+++ src/test/java/com/example/redis_demo/RedisDemoApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.redis_demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootTest
 class RedisDemoApplicationTests {
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @Test
-    void contextLoads() {
+    void testString() {
+        // 写入String数据
+        redisTemplate.opsForValue().set("name","fkh");
+        Object name = redisTemplate.opsForValue().get("name");
+        System.out.println("name = " + name);
     }
 
 }
